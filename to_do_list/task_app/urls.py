b6b8b491e92cd45f8ceff25a46dd335caac4838a
Matias <matias.duran@usach.cl>
2021-10-16 18:17:42 -0300

    add url to block or unblock de task

--- to_do_list/task_app/urls.py
+++ to_do_list/task_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('tag/<int:tag_id>/task/create/', views.create_task, name='create_task'),
     path('tag/<int:tag_id>/task/<int:task_id>/edit_status/', views.edit_status_task, name='edit_status_task'),
     path('tag/<int:tag_id>/task/<int:task_id>/send_backlog/', views.send_backlog_task, name='send_backlog_task'),
+    path('tag/<int:tag_id>/task/<int:task_id>/block/', views.block_task, name='block_task'),
     path('tag/<int:tag_id>/task/archive_all/', views.archive_all_tasks, name='archive_all_tasks'),
     path('tag/<int:tag_id>/show_all_archived/', views.show_all_archived, name='show_all_archived'),
     path('tag/<int:tag_id>/show_bottom_backlog/', views.show_bottom_backlog, name='show_bottom_backlog'),
